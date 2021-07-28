39cf66f2b86660021ffea09bfc4fd73728475564
Usman Qazi <usmanqazi@MacBook-Pro.local>
2021-07-27 18:59:26 -0700

    Fixed home_timeline

--- docs/scripts/init.js
+++ docs/scripts/init.js
@@ -1,7 +1,20 @@
 import * as mockroblog from './mockroblog.js'
 window.mockroblog = mockroblog
 
-let timeline = mockroblog.getPublicTimeline() //Change to get home timeline
+let timeline = null
+
+if (document.querySelector("#home_tl") === document.querySelector('.active'))
+{
+    timeline = mockroblog.getHomeTimeline('ProfAvery') //Change to get home timeline
+}
+else if (document.querySelector("#user_tl") === document.querySelector('.active'))
+{
+    timeline = mockroblog.getUserTimeline('ProfAvery') //Change to get user timeline
+}
+else if (document.querySelector('#public_tl') === document.querySelector('.active'))
+{
+    timeline = mockroblog.getPublicTimeline() //Change to get public timeline
+}
 
 for (let i = 0; i < timeline.length; i++) {
     let timelinePost = document.createElement('div')
@@ -22,16 +35,3 @@ const newPostArea = document.querySelector(".new-post-area")
 newPostBtn.addEventListener("click", () => {
     newPostArea.classList.toggle("hidden")
 })
-
-timeline = mockroblog.getUserTimeline() //Change to get home timeline
-
-for (let i = 0; i < timeline.length; i++) {
-    let timelinePost = document.createElement('div')
-    timelinePost.className = 'p-5 m-5 rounded-lg bg-black hover:bg-purple-700'
-
-    timelinePost.innerHTML += "User " + timeline[i].user_id
-    timelinePost.innerHTML += "<span class = 'float-right'>" + timeline[i].timestamp + "</span>" + "<br>"  + "<hr>"
-    timelinePost.innerHTML += timeline[i].text
-    
-    document.getElementById("user-timeline").append(timelinePost)
-}
