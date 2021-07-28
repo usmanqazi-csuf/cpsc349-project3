1afda63eb28d85aeeca6a7a18acf6aa1762992c0
Manuel Perez <Mperez5891@gmail.com>
2021-07-27 18:01:50 -0700

    User Timeline Update

--- docs/scripts/userTimeline.js
+++ docs/scripts/userTimeline.js
@@ -1,29 +1,7 @@
 import * as mockroblog from './mockroblog.js'
 window.mockroblog = mockroblog
 
-let timeline = mockroblog.getPublicTimeline() //Change to get home timeline
-
-for (let i = 0; i < timeline.length; i++) {
-    let timelinePost = document.createElement('div')
-    timelinePost.className = 'p-5 m-5 rounded-lg bg-black hover:bg-purple-700'
-
-    timelinePost.innerHTML += "User " + timeline[i].user_id
-    timelinePost.innerHTML += "<span class = 'float-right'>" + timeline[i].timestamp + "</span>" + "<br>"  + "<hr>"
-    timelinePost.innerHTML += timeline[i].text
-    
-    document.getElementById("timeline").append(timelinePost)
-}
-
-// New post dropdown menu
-
-const newPostBtn = document.querySelector("button.new-post-button")
-const newPostArea = document.querySelector(".new-post-area")
-
-newPostBtn.addEventListener("click", () => {
-    newPostArea.classList.toggle("hidden")
-})
-
-timeline = mockroblog.getUserTimeline() //Change to get home timeline
+const timeline = mockroblog.getUserTimeline('ProfAvery') //Change to get home timeline
 
 for (let i = 0; i < timeline.length; i++) {
     let timelinePost = document.createElement('div')
@@ -35,3 +13,13 @@ for (let i = 0; i < timeline.length; i++) {
     
     document.getElementById("user-timeline").append(timelinePost)
 }
+
+// New post dropdown menu
+
+const newPostBtn = document.querySelector("button.new-post-button")
+const newPostArea = document.querySelector(".new-post-area")
+
+newPostBtn.addEventListener("click", () => {
+    newPostArea.classList.toggle("hidden")
+})
+
