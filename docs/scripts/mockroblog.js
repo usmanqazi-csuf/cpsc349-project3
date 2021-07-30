202e047a589602d96518a5a27bfd8e161df24b9d
Usman Qazi <usmanqazi@MacBook-Pro.local>
2021-07-29 19:09:39 -0700

    Fixed timestamp for new post

--- docs/scripts/mockroblog.js
+++ docs/scripts/mockroblog.js
@@ -213,7 +213,7 @@ export function postMessage (userId, text) {
       now.getUTCFullYear() + '-' +
       String(now.getUTCMonth() + 1).padStart(2, '0') + '-' +
       String(now.getUTCDate()).padStart(2, '0') + ' ' +
-      String(now.getUTCHours()).padStart(2, '0') + ':' +
+      String(now.getUTCHours() + 5).padStart(2, '0') + ':' +
       String(now.getUTCMinutes()).padStart(2, '0') + ':' +
       String(now.getUTCSeconds()).padStart(2, '0')
 
