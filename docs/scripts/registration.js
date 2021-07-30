4af2136b74f07a66090e101e643e2c5387fa61bd
Stanley Shi <stanleyshi@csu.fullerton.edu>
2021-07-29 20:35:29 -0700

    Added registration functionality

--- docs/scripts/registration.js
+++ docs/scripts/registration.js
@@ -1,6 +1,28 @@
 import * as mockroblog from './mockroblog.js'
 window.mockroblog = mockroblog
 
-// function checkForRegistrationValidation(usernameInput, emailInput, passwordInput, confirmPasswordInput) {
+function checkForRegistrationValidation(usernameInput, emailInput, passwordInput, confirmPasswordInput) {
+    return true
+}
 
-// }
+function register () {
+    if(checkForRegistrationValidation()){
+        const loginSession = window.sessionStorage
+        const usernameInput = document.getElementById('registration-username').value   
+        const emailInput = document.getElementById('registration-email').value
+        const passwordInput = document.getElementById('registration-password').value
+        const userInfo = mockroblog.createUser(usernameInput, emailInput, passwordInput)
+
+        if (userInfo) {
+            // Store login info
+            loginSession.setItem('uid', userInfo.id)
+            loginSession.setItem('username', userInfo.username)
+            window.location.href = 'home_timeline.html'
+        }
+    }
+    else{
+        //display error message
+    }
+}
+const registerButton = document.getElementById('register-button')
+registerButton.onclick = function () { register() }
